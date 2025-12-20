9efe39cbf820270dbe47bbbb3c291008a94bb1e7
tomtomtom <tinges-blotchy2y@icloud.com>
2025-12-20 11:08:41 +0800

    fix mobile layout padding and spacing

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_1c3irc from "../../assets/blog-placeholder-2.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsupplement-schedule-guide.md";
+export default new Map([["../../assets/blog-placeholder-2.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsupplement-schedule-guide.md", __ASTRO_IMAGE_IMPORT_1c3irc]]);
+		
