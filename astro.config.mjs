3f360f4ce37927a52b6e4e53eb67df76a19f7d00
tomtomtom <tinges-blotchy2y@icloud.com>
2025-12-26 18:35:09 +0800

    fix: 修复自定义域名配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tombtc123.github.io',
-	base: '/my-blog',
+	site: 'https://blocktom.com',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		shikiConfig: {
